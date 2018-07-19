338ca6072092e9d31f4ed21b62b1f03b3a6432cc
Slki Shin <slki.shin@samsung.com>
2018-07-19 04:48:08 +0000

    mocha: add signal handler descriptor
    Signed-off-by: Slki Shin <slki.shin@samsung.com>

--- test/test.js
+++ test/test.js
@@ -1,8 +1,22 @@
-var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1,2,3].indexOf(4), -1);
-    });
-  });
+var dbus = require('dbus-native');
+var bus = dbus.systemBus();
+describe('org.bluez', function() {
+	describe('register org.freedesktop.DBus.ObjectManager signal handler', function() {
+		it('InterfacesAdded, InterfacesRemoved', function() {
+			bus.getService('org.bluez').getInterface(
+				'/', 'org.freedesktop.DBus.ObjectManager', function(err, itf) {
+				itf.on('InterfacesAdded', function() {});
+				itf.on('InterfacesRemoved', function() {});
+			});
+		});
+	});
+
+	describe('register org.freedesktop.DBus.Properties signal handler', function() {
+		it('PropertiesChanged', function() {
+			bus.getService('org.bluez').getInterface(
+				'/org/bluez/hci0', 'org.freedesktop.DBus.Properties', function(err, itf) {
+				itf.on('PropertiesChanged', function() {});
+			});
+		});
+	});
 });
