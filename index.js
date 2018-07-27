6ad6dcf80ff8de21d0467fffaa0a4d4e79e99fe7
Slki Shin <slki.shin@samsung.com>
2018-07-27 02:12:27 +0000

    replace dbus-native to node-dbus
    It seems that we cannot register dbus object properly with dbus-native.
    In particular, get property does not work well.
    
    Signed-off-by: Slki Shin <slki.shin@samsung.com>

--- index.js
+++ index.js
@@ -1,56 +1,83 @@
-var dbus = require('dbus-native');
-var bus = dbus.systemBus();
+var DBus = require('dbus');
+
+var bus = DBus.getBus('system');
 var user_cb;
 
 var API = function () {
-	bus.getService('org.bluez').getInterface(
-		'/',
-		'org.freedesktop.DBus.ObjectManager', function(err, itf) {
+	bus.getInterface(
+		'org.bluez',
+		'/org/bluez/hci0',
+		'org.freedesktop.DBus.Properties', function(err, iface) {
 
-		// dbus signals are EventEmitter events
-		itf.on('InterfacesAdded', function() {
-			if (user_cb)
-				user_cb(arguments[1]);
-		});
-
-		itf.on('InterfacesRemoved', function() {
-			console.log('InterfacesRemoved', arguments);
+		iface.on('PropertiesChanged', function(count) {
+			console.log('Discovering: ' + arguments[1].Discovering);
 		});
 	});
 
-	bus.getService('org.bluez').getInterface(
-		'/org/bluez/hci0',
-		'org.freedesktop.DBus.Properties', function(err, itf) {
+	bus.getInterface(
+		'org.bluez',
+		'/',
+		'org.freedesktop.DBus.ObjectManager', function(err, iface) {
 
-		itf.on('PropertiesChanged', function() {
-			// console.log('PropertiesChanged', arguments);
-			arguments[1].forEach(e1 => {
-				console.log(e1[0] + ' : ' + e1[1][1]);
-			});
+		iface.on('InterfacesAdded', function(count) {
+			if (user_cb)
+				user_cb(arguments[1]['org.bluez.Device1']);
+		});
+
+		iface.on('InterfacesRemoved', function(count) {
+			console.log(arguments[1]['org.bluez.Device1'].Address);
 		});
 	});
 }
 
 API.prototype.scan = function(onoff, cb) {
-	var m;
 	if (onoff === 'on') {
-		m = 'StartDiscovery';
 		user_cb = cb;
-	} else {
-		m = 'StopDiscovery';
+		bus.getInterface('org.bluez', '/org/bluez/hci0', 'org.bluez.Adapter1', function(err, iface) {
+			iface.StartDiscovery({}, function(err) {
+				console.log('StartDiscovery');
+			});
+		});
+	} else if (onoff === 'off') {
 		user_cb = null;
+		bus.getInterface('org.bluez', '/org/bluez/hci0', 'org.bluez.Adapter1', function(err, iface) {
+			iface.StopDiscovery({}, function(err) {
+				console.log('StartDiscovery');
+			});
+		});
 	}
+};
 
-	bus.invoke( {
-		path:'/org/bluez/hci0',
-		destination: 'org.bluez',
-		'interface': 'org.bluez.Adapter1',
-		member: m,
-		type: dbus.messageType.methodCall
-	}, function (err) {
-		if (err)
-			cb(null, err);
-	});
+API.prototype.discoverable = function(onoff, cb) {
+	if (onoff === 'on') {
+		user_cb = cb;
+		bus.getInterface('org.bluez', '/org/bluez/hci0', 'org.freedesktop.DBus.Properties', function(err, iface) {
+			iface.Set('org.bluez.Adapter1', 'Discoverable', true, function() {
+			});
+		});
+	} else if (onoff === 'off') {
+		user_cb = null;
+		bus.getInterface('org.bluez', '/org/bluez/hci0', 'org.freedesktop.DBus.Properties', function(err, iface) {
+			iface.Set('org.bluez.Adapter1', 'Discoverable', false, function() {
+			});
+		});
+	}
+};
+
+API.prototype.adv = function(onoff, cb) {
+	if (onoff === 'on') {
+		bus.getInterface('org.bluez', '/org/bluez/hci0', 'org.bluez.LEAdvertisingManager1', function(err, iface) {
+			iface.RegisterAdvertisement('/bluenode/adv', function() {
+				console.log('RegisterAdvertisement');
+			});
+		});
+	} else if (onoff ==='off') {
+		bus.getInterface('org.bluez', '/org/bluez/hci0', 'org.bluez.LEAdvertisingManager1', function(err, iface) {
+			iface.UnregisterAdvertisement('/bluenode/adv', function() {
+				console.log('RegisterAdvertisement');
+			});
+		});
+	}
 };
 
 module.exports = API;
--- sample/scan.js
+++ sample/scan.js
@@ -2,27 +2,6 @@ var API = require('../index');
 var bt = new API();
 
 bt.scan('on', function (d, e) {
-	if (e) {
-		console.log(e);
-		return;
-	}
-
-	d.forEach(e1 => {
-		if (e1[0] === 'org.bluez.Device1') {
-			e1[1].forEach(e2 => {
-				if (e2[0] === 'ManufacturerData') {
-					console.log(e2[0] + ': ');
-					console.log(e2[1][1][0][0][1][1]);
-				} else if (e2[0] === 'ServiceData ') {
-					console.log(e2[0] + ': ');
-					console.log(e2[1][1][0][0][1][1]);
-				} else {
-					console.log(e2[0] + ': ' + e2[1][1]);
-				}
-			})
-		}
-	});
-	console.log();
-
-	api.scan('off');
+	console.log(d);
+	bt.scan('off');
 });
