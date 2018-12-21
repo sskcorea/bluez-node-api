ab6c3e79fd0145f9301db5e59b9f426c95a3b451
Slki Shin <slki.shin@samsung.com>
2018-12-21 02:15:03 +0000

    add readline for the sample app
    Signed-off-by: Slki Shin <slki.shin@samsung.com>

--- sample/s1.js
+++ sample/s1.js
@@ -1,5 +1,8 @@
 var API = require('../index');
+const readline = require('readline');
 var target_addr;
+
+readline.emitKeypressEvents(process.stdin);
 
 var bt = new API( function(evt) {
 	if (evt.name === 'PropertiesChanged' && evt.property === 'Discovering') {
@@ -10,7 +13,7 @@ var bt = new API( function(evt) {
 	} else if (evt.name === 'InterfacesAdded' && evt.property === 'org.bluez.Device1') {
 		if (evt.data.Address === target_addr) {
 			bt.scan('off');
-			bt.pair(target_addr);
+			// bt.pair(target_addr);
 		}
 	} else if (evt.name === 'InterfacesAdded' && evt.property === 'org.bluez.MediaControl1') {
 		console.log(evt.data);
@@ -23,11 +26,30 @@ var bt = new API( function(evt) {
 	}
 });
 
-if (process.argv.length < 3) {
-	console.log('error: provide target address');
-	process.exit(1);
-}
+// if (proces-s.argv.length < 3) {
+// 	console.log('error: provide target address');
+// 	process.exit(1);
+// }
 
-target_addr = process.argv[2];
+// target_addr = process.argv[2];
 
-bt.scan('on');
+process.stdin.setRawMode(true);
+console.log('---------------------');
+console.log('1: scan on');
+console.log('2: scan off');
+console.log('q: exit');
+console.log('---------------------');
+
+process.stdin.on('keypress', (str, key) => {
+	if (key.ctrl && key.name ==='c') {
+		process.exit();
+	} else if (key.name === '1') {
+		bt.scan('on');
+	} else if (key.name === '2') {
+		bt.scan('off');
+	} else if (key.name === 'q') {
+		process.exit();
+	} else {
+		// console.log(`pressed "${str}" key`);
+	}
+});
